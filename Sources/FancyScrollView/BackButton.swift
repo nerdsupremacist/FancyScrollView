1f5f07aa96df330abca48d45d5934e653db0c772
Palle Klewitz <palleklewitz@icloud.com>
2020-06-18 15:28:34 +0200

    Fixes back button visibility outside of navigation stacks

--- Sources/FancyScrollView/BackButton.swift
+++ Sources/FancyScrollView/BackButton.swift
@@ -5,16 +5,23 @@ struct BackButton: View {
 
     @Environment(\.presentationMode)
     var presentationMode: Binding<PresentationMode>
+    
+    @State private var hasBeenShownAtLeastOnce: Bool = false
 
     var body: some View {
-         Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
-             Image(systemName: "chevron.left")
-                 .resizable()
-                 .aspectRatio(contentMode: .fit)
-                 .frame(height: 20, alignment: .leading)
-                 .foregroundColor(color)
-                 .padding(.horizontal, 16)
-                 .font(Font.body.bold())
-         }
+        (presentationMode.wrappedValue.isPresented || hasBeenShownAtLeastOnce) ?
+            Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+               Image(systemName: "chevron.left")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+                   .frame(height: 20, alignment: .leading)
+                   .foregroundColor(color)
+                   .padding(.horizontal, 16)
+                   .font(Font.body.bold())
+            }
+            .onAppear {
+                self.hasBeenShownAtLeastOnce = true
+            }
+        : nil
     }
 }
