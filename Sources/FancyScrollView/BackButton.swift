a5b438090818d53ed73bdf4f78fdde44fa5cc3c4
Palle Klewitz <palleklewitz@icloud.com>
2020-06-18 15:19:27 +0200

    Not hiding button

--- Sources/FancyScrollView/BackButton.swift
+++ Sources/FancyScrollView/BackButton.swift
@@ -7,15 +7,14 @@ struct BackButton: View {
     var presentationMode: Binding<PresentationMode>
 
     var body: some View {
-        presentationMode.wrappedValue.isPresented ?
-            Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
-                Image(systemName: "chevron.left")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 20, alignment: .leading)
-                    .foregroundColor(color)
-                    .padding(.horizontal, 16)
-                    .font(Font.body.bold())
-            } : nil
+         Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+             Image(systemName: "chevron.left")
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .frame(height: 20, alignment: .leading)
+                 .foregroundColor(color)
+                 .padding(.horizontal, 16)
+                 .font(Font.body.bold())
+         }
     }
 }
